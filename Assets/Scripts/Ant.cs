aa9abe6d36a38c2fcd9be40435b5be3b07bef47c
acodedoer <misbahu.s.z@gmail.com>
2022-01-13 18:09:03 +0000

    uploaded

---
+++ Assets/Scripts/Ant.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ant : MonoBehaviour
+{
+    public static float maxSpeed;
+    public static float steerStrength;
+    public static float wanderStrength;
+    public static int wanderRadius;
+    public GameObject prefab;
+    Transform target;
+
+    Vector2 position;
+    Vector2 desiredDirection;
+    Vector2 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //target = prefab.GetComponent<Transform>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(position.x)>wanderRadius || Mathf.Abs(position.y)>wanderRadius)
+        {
+            desiredDirection = ((Vector2.zero - position) + Random.insideUnitCircle * wanderStrength).normalized;   
+        }
+        else
+        {
+            desiredDirection = (desiredDirection + Random.insideUnitCircle * wanderStrength).normalized;
+        }
+        
+        
+        Vector2 desiredVelocity = desiredDirection * maxSpeed;
+        Vector2 accelartion = Vector2.ClampMagnitude((desiredVelocity - velocity) * steerStrength, steerStrength)/1;
+        velocity = Vector2.ClampMagnitude(velocity + accelartion * Time.deltaTime, maxSpeed);
+        position += velocity*Time.deltaTime;
+
+        float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+        transform.SetPositionAndRotation(position, Quaternion.Euler(0,0, angle)); 
+    }
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject ant, camera;
+    public GameObject floor;
+    public float antMaxSpeed = 0;
+    public float antSteerStrength;
+    public float antWanderStrength;
+    public int worldRadius, antPopulation;
+    const float CAMERAPOSITIONRATIO = 1.7f;
+    const int PLANERATIO = 5;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        var instantiatedFloor = Instantiate(floor);
+        instantiatedFloor.transform.localScale = new Vector3(worldRadius/PLANERATIO, 1, worldRadius/ PLANERATIO);
+        instantiatedFloor.transform.position = Vector3.zero;
+        camera.transform.position = new Vector3(0, 0, -worldRadius * CAMERAPOSITIONRATIO);
+        Ant.maxSpeed = antMaxSpeed;
+        Ant.steerStrength = antSteerStrength;
+        Ant.wanderStrength = antWanderStrength;
+        Ant.wanderRadius = worldRadius;
+        for (int i = 0; i < antPopulation; i++)
+        {
+            var instantiatedAnt = Instantiate(ant, Vector3.zero, Quaternion.Euler(-90, 0, 0));
+        }
+    }
+}
